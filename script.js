06b8e781b1a0d28b2dc180be186ba5ced646c9f1
nikhil tripathi <tripathin645@gmail.com>
2020-05-17 20:04:31 +0530

    clock version 2

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ setInterval(setClock,1000);
 const secHand = document.getElementById("sec-hand");
 const minHand = document.getElementById("min-hand");
 const hourHand = document.getElementById("hour-hand");
+const time = document.getElementById("time");
 
 function setClock(){
     const currentDate = new Date();
-
+     time.innerHTML = "TIME : "+Date().slice(16,25);
     const secRatio = currentDate.getSeconds()/60;
     const minRatio = (secRatio + currentDate.getMinutes())/60;
     const hourRatio = ( minRatio + currentDate.getHours())/12;
-    
     setRotation(secHand,secRatio);
     setRotation(minHand,minRatio);
     setRotation(hourHand,hourRatio);
